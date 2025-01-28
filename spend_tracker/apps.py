b73104da0b17369e6865c87d9d32392689a82895
Mykyta Soloviov <mykyta@mykmac.local>
2025-01-28 13:13:24 -0500

    Refactor with Black

--- spend_tracker/apps.py
+++ spend_tracker/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SpendTrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spend_tracker'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "spend_tracker"
