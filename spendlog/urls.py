e81f23b24b2c8117bf5352115b932dda650db05e
mykytaso <mykyta.soloviov@gmail.com>
2025-01-31 14:29:51 -0500

    Include spend_tracker URLs in project’s main URL configuration

--- spendlog/urls.py
+++ spendlog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path("api/user/", include("users.urls", namespace="user")),
+    path("api/spend-tracker/", include("spend_tracker.urls", namespace="money")),
 ]
