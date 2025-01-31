d11234364d9ea5a4d35383cdd3b2fb8c2cb8bd44
mykytaso <mykyta.soloviov@gmail.com>
2025-01-31 12:42:44 -0500

    Add Currency, DefaultCurrency, Unit, Transactions models to spend_tracker app

--- spend_tracker/models.py
+++ spend_tracker/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+from spendlog import settings
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=3, unique=True)
+    rate = models.DecimalField(max_digits=16, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class DefaultCurrency(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    default_currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
+class Unit(models.Model):
+    UNIT_TYPE_CHOICES = [
+        ("INCOME", "Income"),
+        ("EXPENSE", "Expense"),
+        ("ACCOUNT", "Account"),
+    ]
+
+    name = models.CharField(max_length=20)
+    amount = models.DecimalField(max_digits=16, decimal_places=2, default=0)
+    unit_type = models.CharField(max_length=10, choices=UNIT_TYPE_CHOICES)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    in_balance = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="units"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "user"], name="unique_unit_name_per_user"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.unit_type}: {self.name}"
+
+
+class Transaction(models.Model):
+    date_time = models.DateTimeField(auto_now_add=True)
+    source_unit = models.ForeignKey(
+        Unit, on_delete=models.CASCADE, related_name="source_transactions"
+    )
+    destination_unit = models.ForeignKey(
+        Unit, on_delete=models.CASCADE, related_name="destination_transactions"
+    )
+    source_amount = models.DecimalField(max_digits=16, decimal_places=2)
+    destination_amount = models.DecimalField(max_digits=16, decimal_places=2, null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="transactions"
+    )
