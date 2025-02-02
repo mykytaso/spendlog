831918c25679a110fde85babd20025ddf8a48e36
mykytaso <mykyta.soloviov@gmail.com>
2025-02-01 19:19:36 -0500

    Add admin panel functionality

--- spendlog/urls.py
+++ spendlog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/user/", include("users.urls", namespace="user")),
     path("api/spend-tracker/", include("spend_tracker.urls", namespace="money")),
 ]
