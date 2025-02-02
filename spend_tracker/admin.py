3ea1bb3089fab9a565b37a43f6654f1e75cd8773
mykytaso <mykyta.soloviov@gmail.com>
2025-02-02 10:51:47 -0500

    Register Currency, DefaultCurrency, Unit, Transaction models in admin panel

--- spend_tracker/admin.py
+++ spend_tracker/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from spend_tracker.models import Currency, DefaultCurrency, Unit, Transaction
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DefaultCurrency)
+class DefaultCurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
