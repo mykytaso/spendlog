65b93ebd425567f498aa8a79c0d6ff8b7fce4ea6
mykytaso <mykyta.soloviov@gmail.com>
2025-02-01 19:28:32 -0500

    Add update_create_currencies task

--- spend_tracker/tasks.py
+++ spend_tracker/tasks.py
@@ -1,8 +1,8 @@
-from spend_tracker.models import Currency
+from spend_tracker.helpers.currencies import update_or_create_currencies_in_db
 
 from celery import shared_task
 
 
 @shared_task
-def count_currencies() -> int:
-    return Currency.objects.count()
+def update_create_currencies() -> None:
+    update_or_create_currencies_in_db()
